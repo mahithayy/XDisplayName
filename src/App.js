1b1396e71d6f0b8dad462b906d9c5518ca8c1042
mahithayy <hollamahitha@gmail.com>
2025-10-06 20:34:35 +0530

    Initial commit - XDisplayName app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName.trim() && lastName.trim()) {
+      setIsSubmitted(true);
+    } else {
+      setIsSubmitted(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        required
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        name="lastName"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        required
+      />
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+      <p>
+        {isSubmitted
+          ? `${firstName} ${lastName}`
+          : "Please fill all the fields"}
+      </p>
+    </form>
   );
 }
 
-export default App;
+
+
+
